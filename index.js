b3f27b127b46dd0846f4a8989c77879e12d1f7c3
SriNikethanP <srinikethan.ponna@gmail.com>
2024-01-21 15:20:30 +0530

    First working Play and Pause funtion of Song

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
-let AllPlayButtons = document.querySelectorAll(".AudioController").length;
-for (let index = 0; index < AllPlayButtons; index++) {
-   let PlayButton = document.querySelectorAll(".AudioController")[index];
-    PlayButton.addEventListener("click", () => {
-        let currentButton = PlayButton.classList.toggle("pressed");
+// let AllPlayButtons = document.querySelectorAll(".AudioController").length;
+// for (let index = 0; index < AllPlayButtons; index++) {
+//    let PlayButton = document.querySelectorAll(".AudioController")[index];
+//     PlayButton.addEventListener("click", () => {
+//         let currentButton = PlayButton.classList.toggle("pressed");
         
         
-        if (currentButton === true) {
-                let Song = new Audio("PlaylistSongs/Banjaara.mp3");
-                Song.play();
-                // console.log("play")
-                // currentButton = PlayButton.classList.remove("pressed")
-            }
-            else if (currentButton === false){
-                console.log("played")
-                // Song.pause();
-            }
+//         if (currentButton === true) {
+//                 let Song = new Audio("PlaylistSongs/Banjaara.mp3");
+//                 Song.play();
+//                 // console.log("play")
+//                 // currentButton = PlayButton.classList.remove("pressed")
+//             }
+//             else if (currentButton === false){
+//                 console.log("played")
+//                 // Song.pause();
+//             }
 
             
         
 
-        // console.log(PlayButton)
+//         // console.log(PlayButton)
 
+//     })
+// }
+// // document.querySelector(".AudioController").addEventListener("click", play)
+// // function play() {
+    
+// // }
+// // console.log("Audio")
+// // var audio = new Audio("PlaylistSongs/Banjaara.mp3")
+// // audio.play();
+
+let AllPlayButtons = document.querySelectorAll(".AudioController").length;
+let Song = new Audio("PlaylistSongs/Banjaara.mp3")
+for (let index = 0; index < AllPlayButtons; index++) {
+    let PlayButton = document.querySelectorAll(".AudioController")[index];
+    PlayButton.addEventListener("click", () => {
+        if (Song.paused) {
+            Song.play();
+
+        } else {
+            Song.pause();
+        }
+    
     })
 }
-// document.querySelector(".AudioController").addEventListener("click", play)
-// function play() {
-    
-// }
-// console.log("Audio")
-// var audio = new Audio("PlaylistSongs/Banjaara.mp3")
-// audio.play();
