5dc786f9df161ab48360390fcb5976c6221a30ec
SriNikethanP <srinikethan.ponna@gmail.com>
2024-01-21 15:15:07 +0530

    First try to play and pause a song using toggle and class

---
+++ index.js
@@ -0,0 +1,32 @@
+let AllPlayButtons = document.querySelectorAll(".AudioController").length;
+for (let index = 0; index < AllPlayButtons; index++) {
+   let PlayButton = document.querySelectorAll(".AudioController")[index];
+    PlayButton.addEventListener("click", () => {
+        let currentButton = PlayButton.classList.toggle("pressed");
+        
+        
+        if (currentButton === true) {
+                let Song = new Audio("PlaylistSongs/Banjaara.mp3");
+                Song.play();
+                // console.log("play")
+                // currentButton = PlayButton.classList.remove("pressed")
+            }
+            else if (currentButton === false){
+                console.log("played")
+                // Song.pause();
+            }
+
+            
+        
+
+        // console.log(PlayButton)
+
+    })
+}
+// document.querySelector(".AudioController").addEventListener("click", play)
+// function play() {
+    
+// }
+// console.log("Audio")
+// var audio = new Audio("PlaylistSongs/Banjaara.mp3")
+// audio.play();
