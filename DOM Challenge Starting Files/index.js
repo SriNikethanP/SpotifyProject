b0e1fa68634ffcfa3d56d11a87c4890f18ed0e51
SriNikethanP <srinikethan.ponna@gmail.com>
2024-03-02 18:24:34 +0530

    Removed all committed files

--- DOM Challenge Starting Files/index.js
+++
@@ -1,9 +0,0 @@
-    document.firstElementChild.lastElementChild.firstElementChild.firstElementChild.style.color = "red";
-    alert("hello");
-    document.getElementsByTagName("a")[0].style.color = "green";
-    alert("welcome");
-    document.getElementsByClassName("list.a")[0].style.color = "blue";
-    alert("let's see");
-    document.getElementById("item a");
-    alert("bye");
-    document.querySelector("#item .list a")
